da2f3370ecfecc5bc4f80c602c2dbab63ea2d28a
Carl Kakisis <Messinias1@gmail.com>
2020-02-21 23:33:20 -0500

    clesius to farenheit

--- moreCSharpAlgs/Program.cs
+++ moreCSharpAlgs/Program.cs
@@ -10,6 +10,17 @@ namespace moreCSharpAlgs
     {
         static void Main(string[] args)
         {
+            void convertToF(int celsius)
+            {
+                var fahrenheit = 0;
+                Console.WriteLine(fahrenheit = (celsius * 9) / 5 + 32);
+            }
+
+            convertToF(30);
         }
     }
 }
+
+//The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
+
+//You are given a variable celsius representing a temperature in Celsius.Use the variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature.Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.
