f096b9c0127297707dcde181e57aa301ea6bf49a
Carl Kakisis <Messinias1@gmail.com>
2020-02-24 23:54:47 -0500

    near solution

--- moreCSharpAlgs/Program.cs
+++ moreCSharpAlgs/Program.cs
@@ -11,17 +11,17 @@ namespace moreCSharpAlgs
         void diffArray(int[] arr1, int[] arr2)
         {
             var newArr = new List<int>();
-            // Same, same; but different.
-            Console.WriteLine(newArr);
+
+            Console.WriteLine(arr2.Except(arr1));
         }
+        
         static void Main(string[] args)
         {
-            int[] arr1 = { 1, 2, 3, 5};
-            int[] arr2 = { 1, 2, 3, 4, 5 };
+            Program p = new Program();
+
+            p.diffArray(new int[] { 1, 2, 3, 5 }, new int[] { 1, 2, 3, 4, 5 });
         }
     }
 }
 
-//The algorithm to convert from Celsius to Fahrenheit is the temperature in Celsius times 9/5, plus 32.
-
-//You are given a variable celsius representing a temperature in Celsius.Use the variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature.Use the algorithm mentioned above to help convert the Celsius temperature to Fahrenheit.
+//Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both.In other words, return the symmetric difference of the two arrays.
